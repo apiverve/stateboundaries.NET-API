8c5d3c53e528c772e686cb319df2123aee3a30f2
APIVerve Team <hello@apiverve.com>
2025-02-03 15:59:12 -0600

    Minor Version Update

---
+++ StateBoundariesQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class StateBoundariesQueryOptions {
+public string state { get; set; }
+}
+
+}
